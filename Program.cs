e428d3306d783846a4e2f883222e46477873c647
Juan Pablo <juanpablofacultad16@hotmail.com>
2025-05-26 11:15:53 -0300

    Unidas Calculadora V1 Y V2

--- Program.cs
+++ Program.cs
@@ -1,48 +1,98 @@
-﻿
-int op;
-int a; 
-int b;
-
+﻿int op;
+double a;
+double b;
 do
 {
-     Console.WriteLine("Escriba el número a");
+    Console.WriteLine("Escriba el número a");
     string? texta = Console.ReadLine();
-    bool resultaa = int.TryParse(texta, out a);
+    bool resultaa = double.TryParse(texta, out a);
     Console.WriteLine("Escriba el número b");
     string? textb = Console.ReadLine();
-    bool resultab = int.TryParse(textb, out b);
-     Console.WriteLine("Elija la opción");
-    Console.WriteLine("1) Sumar");
-     Console.WriteLine("2) Restar");
-    Console.WriteLine("3) Multiplicar");
-     Console.WriteLine("4) Dividir");
-    Console.WriteLine("0) Salir");
+    bool resultab = double.TryParse(textb, out b);
+    Console.WriteLine("Elija la opción");
+    Console.WriteLine("1)Sumar");
+    Console.WriteLine("2)Restar");
+    Console.WriteLine("3)Multiplicar");
+    Console.WriteLine("4)Dividir");
+    Console.WriteLine("5)Valor Absoluto");
+    Console.WriteLine("6)El cuadrado");
+    Console.WriteLine("7)La raiz cuadrada");
+    Console.WriteLine("8)El Seno");
+    Console.WriteLine("9)El Coseno");
+    Console.WriteLine("10)La parte entera del numero");
+    Console.WriteLine("0)Salir");
     string? texOpc = Console.ReadLine();
     bool resultadoOp = int.TryParse(texOpc, out op);
-     int resultado;
-    switch (op) {
-        case 1: resultado = a + b; Console.WriteLine(resultado);
-           
-            break;
-        case 2: resultado = a - b;
+
+    double resultado;
+
+    switch (op)
+    {
+        case 1:
+            resultado = a + b;
             Console.WriteLine(resultado);
             break;
-        case 3: resultado = a * b;
+        case 2:
+            resultado = a - b;
+            Console.WriteLine(resultado);
+            break;
+        case 3:
+            resultado = a * b;
             Console.WriteLine(resultado);
             break;
         case 4:
-            if (b != 0)  {
+            if (b != 0)
+            {
                 resultado = a / b;
                 Console.WriteLine(resultado);
             }
             else
-            { Console.WriteLine("No puede dividirse por cero");
-            } break;
+            {
+                Console.WriteLine("No puede dividirse por cero");
+            }
+            break;
+        case 5:
+            if (a < 0)
+            {
+                resultado = -a;
+                Console.WriteLine("El valor absoluto de " + a + "es " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("El valor absoluto de " + a + "es " + a);
+            }
+            break;
+        case 6:
+            resultado = a * a;
+            Console.WriteLine("El cuadrado de " + a + "es " + resultado);
+            break;
+        case 7:
+            if (a >= 0)
+            {
+                resultado = Math.Sqrt(a);
+                Console.WriteLine("La raiz cuadrada de " + a + "es " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("No se puede sacar la raiz cuadrada de un numero negativo");
+            }
+            break;
+        case 8:
+            resultado = Math.Sin(a);
+            Console.WriteLine("El seno de " + a + "es " + resultado);
+            break;
+        case 9:
+            resultado = Math.Cos(a);
+            Console.WriteLine("El Coseno de " + a + "es " + resultado);
+            break;
+        case 10:
+            resultado = Math.Truncate(a);
+            Console.WriteLine("La parte entera de " + a + "es " + resultado);
+            break;
         case 0:
             Console.WriteLine("Saliendo del programa...");
             break;
-        default:
-            Console.WriteLine("Opción no válida");
-            break;
     }
+
 } while (op != 0);
+
--- obj/Debug/net8.0/tl1-tp6-2025-JuanPabloGonzalez16.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-tp6-2025-JuanPabloGonzalez16.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp6-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d6154d11e05e8fe1a15f5a4c16e1ce325158d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d623076c56288af38e07fb6652aa6c73f723956d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp6-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp6-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
