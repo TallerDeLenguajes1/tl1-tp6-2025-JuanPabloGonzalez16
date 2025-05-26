d623076c56288af38e07fb6652aa6c73f723956d
Juan Pablo <juanpablofacultad16@hotmail.com>
2025-05-26 11:06:10 -0300

    CalculadoraV1 COMMIT

--- Program.cs
+++ Program.cs
@@ -1,62 +1,48 @@
-﻿int op;
-double a;
+﻿
+int op;
+int a; 
+int b;
+
 do
 {
-    Console.WriteLine("Escriba el número a");
+     Console.WriteLine("Escriba el número a");
     string? texta = Console.ReadLine();
-    bool resultaa = double.TryParse(texta, out a);
-    Console.WriteLine("Elija la opción");
-    Console.WriteLine("1)Valor Absoluto");
-    Console.WriteLine("2)El cuadrado");
-    Console.WriteLine("3)La raiz cuadrada");
-    Console.WriteLine("4)El Seno");
-    Console.WriteLine("5)El Coseno");
-    Console.WriteLine("6)La parte entera del numero");
-    Console.WriteLine("0)Salir");
+    bool resultaa = int.TryParse(texta, out a);
+    Console.WriteLine("Escriba el número b");
+    string? textb = Console.ReadLine();
+    bool resultab = int.TryParse(textb, out b);
+     Console.WriteLine("Elija la opción");
+    Console.WriteLine("1) Sumar");
+     Console.WriteLine("2) Restar");
+    Console.WriteLine("3) Multiplicar");
+     Console.WriteLine("4) Dividir");
+    Console.WriteLine("0) Salir");
     string? texOpc = Console.ReadLine();
     bool resultadoOp = int.TryParse(texOpc, out op);
-
-    double resultado;
-
-    switch (op)
-    {
-        case 1:
-            if (a < 0)
-            {
-                resultado = -a;
-                Console.WriteLine("El valor absoluto de " + a + "es " + resultado);
-            }else
-            {
-                Console.WriteLine("El valor absoluto de " + a + "es " + a);
-            }
+     int resultado;
+    switch (op) {
+        case 1: resultado = a + b; Console.WriteLine(resultado);
+           
             break;
-        case 2:
-            resultado = a * a;
-            Console.WriteLine("El cuadrado de " + a + "es " + resultado);
+        case 2: resultado = a - b;
+            Console.WriteLine(resultado);
             break;
-        case 3:
-            if (a >= 0)
-            {
-                resultado = Math.Sqrt(a);
-                Console.WriteLine("La raiz cuadrada de " + a + "es " + resultado);
-            }
-            else
-            {
-                Console.WriteLine("No se puede sacar la raiz cuadrada de un numero negativo");
-            }
+        case 3: resultado = a * b;
+            Console.WriteLine(resultado);
             break;
         case 4:
-            resultado = Math.Sin(a);
-            Console.WriteLine("El seno de " + a + "es " + resultado);
+            if (b != 0)  {
+                resultado = a / b;
+                Console.WriteLine(resultado);
+            }
+            else
+            { Console.WriteLine("No puede dividirse por cero");
+            } break;
+        case 0:
+            Console.WriteLine("Saliendo del programa...");
             break;
-        case 5:
-            resultado = Math.Cos(a);
-            Console.WriteLine("El Coseno de " + a + "es " + resultado);
-            break;
-        case 6:
-            resultado = Math.Truncate(a);
-            Console.WriteLine("La parte entera de " + a + "es " + resultado);
+        default:
+            Console.WriteLine("Opción no válida");
             break;
     }
-
 } while (op != 0);
