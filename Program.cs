8d75e3caa408bfd0bf5cfbd120e3d23c172d4d86
Juan Pablo <juanpablofacultad16@hotmail.com>
2025-05-26 13:58:23 -0300

    Ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Empezando EL ejercicio4");
+﻿Console.WriteLine("Ingrese el numero");
+int a;
+string? texto;
+texto = Console.ReadLine();
+bool resultado = int.TryParse(texto, out a);
+if (resultado)
+{
+    while (a > 0)
+    {
+        Console.Write(a % 10);
+        a /= 10;
+    }
+}
--- obj/Debug/net8.0/tl1-tp6-2025-JuanPabloGonzalez16.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-tp6-2025-JuanPabloGonzalez16.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp6-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2ac1cfab487171c2a48a302bc3f3a39e00f6405")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f43039caf10ac41467298c46cc1d475750ff8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp6-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp6-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
